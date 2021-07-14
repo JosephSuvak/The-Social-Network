3f7710fc66b35a36b26ecdc21261b0274469d4da
William Berry <76190311+wberry86@users.noreply.github.com>
2021-07-14 11:25:50 -0400

    updated routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const commentRoutes = require('./thought-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
