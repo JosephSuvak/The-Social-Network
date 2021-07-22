1a5c5023aaaf13276a4b5e06b39a837b0aec515e
Joseph Suvak <robert.suvak@hcahealthcare.com>
2021-07-21 22:47:13 -0500

    Revert connection.js & package info

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/socialNetworker',
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
